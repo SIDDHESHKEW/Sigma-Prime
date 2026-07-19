27f3a65608dd55ded336b9a6cbbbdd40fa334a3e
Siddhesh <siddhesh1307kewate@gmail.com>
2026-07-19 11:27:42 +0530

    Adding all content to github

---
+++ DSA/learning c++/variables_and_dtype.cpp
@@ -0,0 +1,32 @@
+// #include <iostream>        //<-- 
+// #define PI 3.14            //<-- preprocessor directive
+// using namespace std;
+// int main(){
+//     cout <<PI;
+//     return 0;
+// }
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<< "****\n***\n**\n*\n";  //<<-- printing patterns
+    cout<< "a ="<<a<<"\n\n";            //<-- garbage value of created variable
+
+    cout<<"Size of int ="<<sizeof(int)<<endl;      //<<--DataTypes
+    cout<<"Size of char ="<<sizeof(char)<<endl;
+    cout<<"Size of bool ="<<sizeof(bool)<<endl;
+    cout<<"Size of float ="<<sizeof(float)<<endl;
+    cout<<"Size of double ="<<sizeof(double)<<endl;
+
+
+
+
+    return 0;
+}
+
